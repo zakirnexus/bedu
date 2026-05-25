1177d3112ff8492b4b0e5727041e529c6fbda6c0
zakirnexus <64505844+zakirnexus@users.noreply.github.com>
2026-05-25 17:33:07 +0530

    Add files via upload

---
+++ ViewComponents/NavViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Services;
+
+namespace SchoolProject.ViewComponents
+{
+    public class NavViewComponent : ViewComponent
+    {
+        private readonly NavService _navService;
+
+        public NavViewComponent(NavService navService)
+        {
+            _navService = navService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _navService.GetSchoolNavItems("bangalore");
+            return View(items);
+        }
+    }
+}
